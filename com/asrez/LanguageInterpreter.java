1268b2d5fbc25ac0cc3e5b7b9ea0d978204bbcee
Max Base <Max@Asrez.com>
2019-12-07 21:54:16 +0330

    Update and rename LanguageInterpreter.java to com/asrez/LanguageInterpreter.java

--- com/asrez/LanguageInterpreter.java
+++ com/asrez/LanguageInterpreter.java
@@ -28,12 +28,30 @@ public class LanguageInterpreter {
         return -1;
     }
 
-    public static String parser(String input) throws Exception {
+    public static Boolean isValid(String input) {
+        int i;
+        for(i=0;i<input.length();i++) {
+            if(checkType(input.charAt(i)) == 0) {
+                if(input.length() > i+2 && input.charAt(i+1) == 'o' && input.charAt(i) == input.charAt(i+2)) {
+                    i+=2;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        if(i == input.length()) {
+            return true;
+        }
+        return false;
+    }
+
+    public static String toEnglish(String input) throws Exception {
         String temp="";
         for(int i=0;i<input.length();i++) {
             temp+=input.charAt(i);
             if(checkType(input.charAt(i)) == 0) {
-                if(input.charAt(i+1) == 'o' && input.charAt(i) == input.charAt(i+2)) {
+                if(input.length() > i+2 && input.charAt(i+1) == 'o' && input.charAt(i) == input.charAt(i+2)) {
                     i+=2;
                 }
                 else {
@@ -46,9 +64,10 @@ public class LanguageInterpreter {
 
     public static void main(String arguments[]) {
         try {
-            System.out.println(parser("HoHeyoy alollol, hohelollolo hohowow arore yoyou koknonowowloledodgoge!"));
+            System.out.println(toEnglish("HoHeyoy alollol, hohelollolo hohowow arore yoyou koknonowowloledodgoge!"));
         } catch (Exception e) {
             e.printStackTrace();
         }
+        System.out.println(isValid("HoHeyoy alollol, hohelollolo hohowow arore yoyou koknonowowloledodgoge!"));
     }
 }
